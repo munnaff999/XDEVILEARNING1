5a82b6bdf4920522f81fb580e7dbb7e37f969f17
munnaff999 <ta398479@gmail.com>
2026-01-22 12:32:11 +0530

    Migrate Firebase script to ES module imports

--- firebase.js
+++ firebase.js
@@ -1,14 +1,30 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+// Firebase App (the core Firebase SDK)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
-<script>
+// Firebase Authentication
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+// Firebase Realtime Database
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBUfLZ0hiR0uPQOSyrgrhBZnHf-h3UxezE",
   authDomain: "xdevilearning99.firebaseapp.com",
   databaseURL: "https://xdevilearning99-default-rtdb.firebaseio.com",
   projectId: "xdevilearning99",
+  storageBucket: "xdevilearning99.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-firebase.initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+const auth = getAuth(app);
+const db = getDatabase(app);
+const provider = new GoogleAuthProvider();
+
+// Export for other files
+export { auth, db, provider };
